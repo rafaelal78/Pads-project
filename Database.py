f87a0eee20a4957422195a188dfb9fedd1c134bb
Bobby McBobby Bob Bobbinston <66316255+rafaelal78@users.noreply.github.com>
2022-10-18 18:40:50 +0800

    Update Database.py

--- Database.py
+++ Database.py
@@ -3,16 +3,6 @@ import sqlite3
 conn = sqlite3.connect("PIMS")
 c = conn.cursor()
 
-c.execute('''CREATE TABLE item (
-                Item_name text,
-                Item_cost integer,
-                Number_of_items integer,
-                Date_of_purchase text,
-                Threshold_value integer,
-                Description text,
-                Item_type text
-                )''')
-
 c.execute('''CREATE TABLE employee (
             employee_ID integer,
             full_name text,
@@ -21,6 +11,24 @@ c.execute('''CREATE TABLE employee (
             date_of_birth text,
             employee_type text,
             loan_total_cost integer
+            )''')
+
+c.execute('''CREATE TABLE item (
+                item_ID integer,
+                item_name text,
+                item_cost integer,
+                number_of_items integer,
+                date_of_purchase text,
+                threshold_value integer,
+                description text,
+                item_type text
+                )''')
+
+c.execute('''CREATE TABLE loaned_item (
+            item_ID integer,
+            employee_ID integer,
+            date_of_request text,
+            request_amount integer
             )''')
 
 conn.close()
